27cf127019a2164c69f57a14a13e701a833dc2f6
Rafael Froeschlin Filho <rafael.froe.filho@gmail.com>
2019-04-29 18:17:55 -0300

    Finished basic repo setup

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info : & PanicInfo) -> !{
+	loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> !{
+
+	letvga_p = 0xB8000 as *mut u8;
+
+	let a: &[u8] = b "I am working btw";
+
+	for (i, & b)  in a.iter().enumerate() {
+		unsafe { 
+			* vga_p.offset(i as isize * 2) = b;
+			* vga_p.offset(i as isize * 2 + 1) = 0xB;
+		}
+	}
+
+
+	loop {}
+}
