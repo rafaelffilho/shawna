f36581ac5b96b57d372a6ea5fc89535a2bcfd631
Rafael Froeschlin Filho <rafael.froe.filho@gmail.com>
2019-04-29 18:18:47 -0300

    Fixed typo

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,18 @@
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_info : & PanicInfo) -> !{
+fn panic(_info : & PanicInfo) -> ! {
 	loop {}
 }
 
 #[no_mangle]
-pub extern "C" fn _start() -> !{
+pub extern "C" fn _start() -> ! {
 
-	letvga_p = 0xB8000 as *mut u8;
+	let vga_p = 0xB8000 as *mut u8;
 
 	let a: &[u8] = b "I am working btw";
 
-	for (i, & b)  in a.iter().enumerate() {
+	for (i, &b) in a.iter().enumerate() {
 		unsafe { 
 			* vga_p.offset(i as isize * 2) = b;
 			* vga_p.offset(i as isize * 2 + 1) = 0xB;
