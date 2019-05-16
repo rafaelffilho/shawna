e244ff6b0258b897e5ea403d5fc8e6a3dec250e5
Rafael Froeschlin Filho <rafael.froe.filho@gmail.com>
2019-05-16 20:25:44 -0300

    Handle time interrupts

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,14 +1,32 @@
+#![allow(non_camel_case_types)]
+
 use x86_64::structures::idt::InterruptDescriptorTable;
 use x86_64::structures::idt::InterruptStackFrame;
 use lazy_static::lazy_static;
 use pic8259_simple::ChainedPics;
 use spin;
 
-use crate::println;
+use crate::{print, println};
 use crate::gdt;
 
 pub const PIC_1_OFFSET: u8 = 32;
 pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
+
+#[derive(Debug, Clone, Copy)]
+#[repr(u8)]
+pub enum InterruptIndex {
+	timer = PIC_1_OFFSET
+}
+
+impl InterruptIndex {
+	fn as_u8(self) -> u8 {
+		self as u8
+	}
+
+	fn as_usize(self) -> usize {
+		usize::from(self.as_u8())
+	}
+}
 
 pub static PICS: spin::Mutex<ChainedPics> =
 		spin::Mutex::new(
@@ -16,7 +34,6 @@ pub static PICS: spin::Mutex<ChainedPics> =
 				ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET)
 			}
 		);
-
 
 lazy_static! {
 	static ref IDT: InterruptDescriptorTable = {
@@ -26,12 +43,23 @@ lazy_static! {
 			idt.double_fault.set_handler_fn(double_fault).
 				set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
 		}
+		idt[InterruptIndex::timer.as_usize()].set_handler_fn(timer);
 		idt
 	};
 }
 
 pub fn init_idt () {
 	IDT.load();
+}
+
+extern "x86-interrupt" fn timer (
+	_stack_frame: &mut InterruptStackFrame
+) {
+	print!(".");
+
+	unsafe {
+		PICS.lock().notify_end_of_interrupt(InterruptIndex::timer.as_u8());
+	}
 }
 
 extern "x86-interrupt" fn double_fault (
